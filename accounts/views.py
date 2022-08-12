4aacbaf51efb8a71f203b256832a2080cb9a6d09
krishnashenoy123 <krishnashenoy123@gmail.com>
2022-08-12 09:56:41 +0530

    views for create, delete and update order

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
+from .forms import OrderForm
 
 # Create your views here.
 
@@ -31,3 +33,40 @@ def customer(request, pk):
     order_count = orders.count()
     context = {'customers':customers, 'orders':orders, 'order_count':order_count}
     return render(request, 'accounts/customer.html', context)
+
+def create_order(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10) 
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    #form = OrderForm(initial={'customer':customer})
+    if request.method == 'POST':
+        #print('Printing POST', request.POST)
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'formset':formset}
+    return render(request, 'accounts/order_form.html', context)
+
+def update_order(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        #print('Printing POST', request.POST)
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'accounts/order_form.html', context)
+
+def delete_order(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {'item':order}
+    return render(request, 'accounts/delete.html', context)
